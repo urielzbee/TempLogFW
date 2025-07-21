1146b9c1b58b9c8d644360f1d6505578cb6e98c7
Uriel Zazueta <uriel.zazueta.barreras@gmail.com>
2025-07-20 20:55:22 -0600

    Add LOG to time service. Update time service parameter

--- src/time_service.c
+++ src/time_service.c
@@ -1,5 +1,10 @@
 #include "time_service.h"
 #include <stdio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(time_service);
+
+static const struct device * time_rtc;
 
 static void time_service_callback(const struct device *dev, uint16_t id, void *user_data)
 {
@@ -9,6 +14,9 @@ static void time_service_callback(const struct device *dev, uint16_t id, void *u
 int time_service_init(const struct device * rtc)
 {
     int ret;
+
+	time_rtc = rtc;
+
 	struct rtc_time init_tm = {
 		.tm_year = 2025 - 1900,
 		.tm_mon = 4 - 1,
@@ -28,55 +36,55 @@ int time_service_init(const struct device * rtc)
 		.tm_sec = 0,
 	};
 
-    if (!device_is_ready(rtc)) {
-		printf("RTC is not ready\n");
+    if (!device_is_ready(time_rtc)) {
+		LOG_INF("RTC is not ready\n");
 		return 0;
 	}
 
-    ret = rtc_set_time(rtc, &init_tm);
+    ret = rtc_set_time(time_rtc, &init_tm);
 	if (ret < 0) {
-		printf("Init - Cannot write date time: %d\n", ret);
+		LOG_INF("Init - Cannot write date time: %d\n", ret);
 		return 0;
 	}
 
-    ret = rtc_alarm_set_time(rtc, 0, RTC_ALARM_TIME_MASK_SECOND, &tm);
+    ret = rtc_alarm_set_time(time_rtc, 0, RTC_ALARM_TIME_MASK_SECOND, &tm);
     if (ret < 0) {
-		printf("Cannot set alarm: %d\n", ret);
+		LOG_INF("Cannot set alarm: %d\n", ret);
 		return 0;
 	}
 
-    ret = rtc_alarm_set_callback(rtc, 0, time_service_callback, NULL);
+    ret = rtc_alarm_set_callback(time_rtc, 0, time_service_callback, NULL);
     if (ret < 0) {
-		printf("Cannot set alarm callback: %d\n", ret);
+		LOG_INF("Cannot set alarm callback: %d\n", ret);
 		return 0;
 	}
 
     return 1;
 }
 
-int time_service_set_date_time(const struct device *rtc, struct rtc_time *tm)
+int time_service_set_date_time( struct rtc_time *tm)
 {
 	int ret = 0;
 
-	ret = rtc_set_time(rtc, tm);
+	ret = rtc_set_time(time_rtc, tm);
 	if (ret < 0) {
-		printf("Cannot write date time: %d\n", ret);
+		LOG_INF("Cannot write date time: %d\n", ret);
 		return ret;
 	}
 	return ret;
 }
 
-int time_service_get_date_time(const struct device *rtc, struct rtc_time *tm)
+int time_service_get_date_time( struct rtc_time *tm)
 {
 	int ret = 0;
 
-	ret = rtc_get_time(rtc, tm);
+	ret = rtc_get_time(time_rtc, tm);
 	if (ret < 0) {
-		printf("Cannot read date time: %d\n", ret);
+		LOG_INF("Cannot read date time: %d\n", ret);
 		return ret;
 	}
 
-	printf("%04d-%02d-%02d %02d:%02d:%02d", tm->tm_year + 1900,
+	LOG_INF("%04d-%02d-%02d %02d:%02d:%02d", tm->tm_year + 1900,
 	       tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 
 	return ret;
--- src/time_service.h
+++ src/time_service.h
@@ -5,7 +5,7 @@
 #include <zephyr/drivers/rtc.h>
 
 int time_service_init(const struct device * rtc);
-int time_service_set_date_time(const struct device *rtc, struct rtc_time *tm);
-int time_service_get_date_time(const struct device *rtc, struct rtc_time *tm);
+int time_service_set_date_time( struct rtc_time *tm);
+int time_service_get_date_time( struct rtc_time *tm);
 
 #endif
