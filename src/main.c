38e96fe68f22ec22c1c58e524b11e5b45d633f33
Uriel Zazueta <uriel.zazueta.barreras@gmail.com>
2025-08-04 12:37:37 -0600

    Refactor main.c to simplify board initialization and remove unused logging structures

--- src/main.c
+++ src/main.c
@@ -16,31 +16,10 @@
 #include "telemetry_service.h"
 #include "flash_manager.h"
 #include "command_handler.h"
+#include "temperature_logger_controller.h"
 
 LOG_MODULE_REGISTER(main);
 
-typedef struct 
-{
-	uint32_t magicWord;
-	uint32_t index;
-}tempLogHeader;
-
-typedef struct 
-{
-	uint32_t magicWord;
-	struct rtc_time time;
-	struct sensor_value temp;
-	uint8_t reserved[12];
-}tempLog;
-
-#define TEMP_LOG_HEADER_ADDRESS 0x00000000
-#define TEMP_LOG_START_ADDRESS 0x1000
-#define TEMP_LOG_ERASE_SIZE 0x1000
-#define TEMP_LOG_CAPACITY 0x200000 /* 2MBytes */
-#define TEMP_LOG_SIZE sizeof(tempLog)
-#define TEMP_LOG_HEADER_SIZE sizeof(tempLogHeader)
-#define TEMP_LOG_MAX_LOGS ((TEMP_LOG_CAPACITY - TEMP_LOG_START_ADDRESS) / TEMP_LOG_HEADER_SIZE)
-#define TEMP_LOG_MAGIC_WORD 0xBADDCAFE
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -66,9 +45,6 @@ const struct device *const rtc1 = DEVICE_DT_GET(DT_ALIAS(rtc1));
 const struct device *const mco = DEVICE_DT_GET(DT_NODELABEL(mco));
 const struct device *const telemetry_uart = DEVICE_DT_GET(DT_ALIAS(telemetry_uart));
 
-bool read_data_flag = false;
-
-tempLogHeader header = {0};
 
 void hard_fault(void);
 void board_init(void);
@@ -84,6 +60,44 @@ void hard_fault(void)
 }
 void board_init(void)
 {
+	int ret;
+
+	if (!gpio_is_ready_dt(&led0)) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+	if (!gpio_is_ready_dt(&led1)) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+	if (!gpio_is_ready_dt(&btn1)) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+
+	if (!device_is_ready(mco)) {
+		LOG_ERR("MCO1 device not ready");
+		hard_fault();
+	}
+
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+
+	ret = gpio_pin_configure_dt(&btn1, GPIO_INPUT);
+	if (ret < 0) {
+		LOG_ERR("Device is not ready");
+		hard_fault();
+	}
+
 	if(!sensor_manager_init(dev))
 	{
 		hard_fault();
@@ -102,50 +116,12 @@ void board_init(void)
 
 	command_handler_init();
 
+	temperature_logger_controller_init();
+	temperature_logger_controller_start();
+
 }
 
-static int set_date_time(const struct device *rtc)
-{
-	int ret = 0;
-	struct rtc_time tm = {
-		.tm_year = 2025 - 1900,
-		.tm_mon = 4 - 1,
-		.tm_mday = 18,
-		.tm_hour = 14,
-		.tm_min = 57,
-		.tm_sec = 0,
-	};
-
-	ret = rtc_set_time(rtc, &tm);
-	if (ret < 0) {
-		LOG_ERR("Cannot write date time: %d", ret);
-		return ret;
-	}
-	return ret;
-}
-
-static int get_date_time(const struct device *rtc, struct rtc_time *tm)
-{
-	int ret = 0;
-
-	ret = rtc_get_time(rtc, tm);
-	if (ret < 0) {
-		LOG_ERR("Cannot read date time: %d", ret);
-		return ret;
-	}
-
-	LOG_INF("%04d-%02d-%02d %02d:%02d:%02d", tm->tm_year + 1900,
-	       tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
-
-	return ret;
-}
-
-void rtc_callback(const struct device *dev, uint16_t id, void *user_data)
-{
-	read_data_flag = true;
-}
-
-void print_logs(void)
+/*void print_logs(void)
 {
 	tempLog temperatureLog;
 
@@ -164,94 +140,19 @@ void print_logs(void)
 			break;
 		}
 	}
-}
+}*/
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 	
-	
-	// Initializing devices
-
-	if (!gpio_is_ready_dt(&led0)) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-	if (!gpio_is_ready_dt(&led1)) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-	if (!gpio_is_ready_dt(&btn1)) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-
 	board_init();
-	
-	
 
-	if (!device_is_ready(mco)) {
-		LOG_ERR("MCO1 device not ready");
-		return -1;
-	}
-
-
-	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&btn1, GPIO_INPUT);
-	if (ret < 0) {
-		LOG_ERR("Device is not ready");
-		return 0;
-	}
-
-	set_date_time(rtc1);
-
-
-	// Set RTC Alarm
-	struct rtc_time tm = {
-		.tm_year = 0,
-		.tm_mon = 0,
-		.tm_mday = 0,
-		.tm_hour = 0,
-		.tm_min = 0,
-		.tm_sec = 0,
-	};
-	rtc_alarm_set_time(rtc1, 0, RTC_ALARM_TIME_MASK_SECOND, &tm);
-	rtc_alarm_set_callback(rtc1, 0, rtc_callback, NULL);
-
-	tempLog temperatureLog;
-
-	get_date_time(rtc1, &temperatureLog.time);
-	sensor_manager_read(&temperatureLog.temp);
-	//gpio_pin_set_dt(&led0, 0);
-	//gpio_pin_set_dt(&led1, 0);
 	while (1) {
 		gpio_pin_set_dt(&led0, 1);
 		k_msleep(70);
 		gpio_pin_set_dt(&led0, 0);
-		if(read_data_flag)
-		{
-			read_data_flag = false;
-			get_date_time(rtc1, &temperatureLog.time);
-			sensor_manager_read(&temperatureLog.temp);	
-			temperatureLog.magicWord = FLASH_MANAGER_MAGIC_WORD;
-			flash_manager_write(flash_dev, &temperatureLog, sizeof(tempLog));
-		}
-		if(gpio_pin_get_dt(&btn1))
-		{
-			print_logs();
-		}
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
